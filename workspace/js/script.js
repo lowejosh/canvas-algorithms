fc0e5ea68a7e87cae42a93984defd7ec82d98b70
Your name <lowejosh50@gmail.com>
2017-09-15 23:43:14 +1000

    sierpinski carpet working but needs optimization

--- workspace/js/script.js
+++ workspace/js/script.js
@@ -1,45 +1,39 @@
-// Sierpinksi Carpet
+// Canvas 
 var c = document.getElementById("sierpinskiCarpet"); 
 var ctx = c.getContext("2d");
-var iterations = 2
-ctx.fillStyle="#FFFFFF";
 
-// Initial square
-var square = c.width/3;
-ctx.fillRect(square, square, square, square);
+// Vars
+var squareLength = c.width;
+var iterations = 3;
 
-// Helper vars
-var x1, x2, x3, y1, y2, y3;
+function sierpinskiCarpet(x1, y1, squareLength, iterations){
+    if (iterations == 0) {
+        drawSquare(x1, y1, squareLength);    
+    } else {
+        squareLength = squareLength / 3;
+        var x2 = x1 + squareLength; 
+        var y2 = y1 + squareLength; 
+        var x3 = x1 + squareLength * 2; 
+        var y3 = y1 + squareLength * 2; 
 
-
-for (var i = 0; i < iterations; i++) {
-    prevSquare = square;
-    square = square/3;
-
-    for (var ii = 0; ii < 9; ii++) {
-        for (var k = 0; k <= i; k++) {
-            for (var kk = 0; kk < 9; kk++) {
-                document.write("ReEEE");
-                x1 = (kk * 9 * square) + (3 * ii * square) + square;
-                x2 = (kk * 9 * square) + (3 * (ii - 3) * square) + square;
-                x3 = (kk * 9 * square) + (3 * (ii - 6) * square) + square;
-                y1 = (kk * 9 * square) + square;
-                y2 = (k * 9 * square) + 4 * square;
-                y3 = (k * 9 * square) + 7 * square;
-                if (ii != 4) {
-                    if (ii < 3) {
-                        ctx.fillRect(x1, y1, square, square);
-                    }
-                    else if (ii >= 3 && ii < 6) {
-                        ctx.fillRect(x2, y2, square, square);
-                    }
-                    else {
-                        ctx.fillRect(x3, y3, square, square);
-                    }
-                }
-            
-            }
-        }
-    }    
-    
+        sierpinskiCarpet(x1, y1, squareLength, iterations - 1);
+        sierpinskiCarpet(x1, y2, squareLength, iterations - 1);
+        sierpinskiCarpet(x1, y3, squareLength, iterations - 1);
+        sierpinskiCarpet(x2, y1, squareLength, iterations - 1);
+        sierpinskiCarpet(x2, y3, squareLength, iterations - 1);
+        sierpinskiCarpet(x3, y1, squareLength, iterations - 1);
+        sierpinskiCarpet(x3, y2, squareLength, iterations - 1);
+        sierpinskiCarpet(x3, y3, squareLength, iterations - 1);
+    }
 }
+
+function drawSquare(x1, y1, squareLength) {
+    ctx.fillRect(x1, y1, squareLength, squareLength);
+}
+
+
+ctx.fillStyle="#FFFFFF";
+drawSquare(0, 0, c.width);
+
+ctx.fillStyle="#101010";
+sierpinskiCarpet(0, 0, squareLength, iterations);
