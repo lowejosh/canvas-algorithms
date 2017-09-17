2cd2e12ce9c3acae0929dc864d77afb7718a02f5
Your name <lowejosh50@gmail.com>
2017-09-18 02:15:52 +1000

    finished sierpinski triangle

--- workspace/js/sierpinski-triangle.js
+++ workspace/js/sierpinski-triangle.js
@@ -4,81 +4,73 @@ var ctx = c.getContext("2d");
 ctx.fillStyle = "#FFFFFF";
 
 // Vars
-var iterations = 3;
-var length = c.width / 3;
+var iterations = 4;
+var length = c.width / 2;
 
-// Recursive function that calls upon itself until it reaches the set amount of iterations
 function iteration(x, y, iterationCount) {
-    // If the iteration count is yet to reach the set iterations
     if (iterationCount != iterations + 1) {
-
-        // Retrieve the square size and draw it
-        square = getSquareSize(iterationCount);
-        ctx.fillRect(x, y, square, square);
-        
-        /*
-        // For every sub square
-        for (var i = 0; i < 8; i++) {
-            // If the current iteration isn't the last
+        length = getLength(iterationCount);
+        drawTriangle(x, y);
+        for (var i = 0; i < 3; i++) {
             if (iterationCount + 1 != iterations + 1) {
-                // Retrieve the square size again for sanity
-                square = getSquareSize(iterationCount + 1);
-                // Return each x and y value for each square position
-                var rx = getParamX(i, square, x);
-                var ry = getParamY(i, square, y);
-                // Recursively call the function for the next iteration
+                length = getLength(iterationCount);
+                var rx = getParamX(i, x);
+                var ry = getParamY(i, y);
                 iteration(rx, ry, iterationCount + 1);
             }
-        } */
+        }
     }
 }
 
-// Draws an equilateral triangle
+// Draws an equilateral triangle at the center
+function drawTriangle(x, y) {
+    x-=c.width/4; // centers the triangle x axis
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + length, y);
+    ctx.lineTo(x + length/2, y + length);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+}
 
-// Returns the x value for proper square placement
-function getParamX(i, squareSize, x) {
-       if (i == 0 || i == 3 || i == 5) {
-            return (x - (2 * square));
-       } else if (i == 1 || i == 6) {
-            return (x + square);
+// Returns the x value for proper length placement
+function getParamX(i, x) {
+       if (i == 0) {
+            return (x + length/4);
+       } else if (i == 1) {
+            return (x - length/4);
        } else {
-            return (x + (4 * square));
+            return (x + 3 * length/4);
        }
 }
 
-// Returns the y value for proper square placement
-function getParamY(i, squareSize, y) {
-       if (i < 3) {
-            return (y - (2 * square));
-       } else if (i < 5) {
-            return (y + square);
+// Returns the y value for proper length placement
+function getParamY(i, y) {
+       if (i == 0) {
+            return (y - length/2);
        } else {
-            return (y + (4 * square));
+            return (y + length/2);
        }
 }
 
-// Returns the square size determined from the current iteration
-function getSquareSize(iterationCount) {
-    return c.width / Math.pow(3, iterationCount);
+// Returns the length size determined from the current iteration
+function getLength(iterationCount) {
+    return c.width / Math.pow(2, iterationCount);
 }
 
-/*
+
 // Redraws the sierpinski carpet with a new iteration setting
 function reDraw(iterationChoice) {
-    // Clear the screen
     ctx.fillStyle = "#101010";
     ctx.fillRect(0, 0, c.width, c.height);
     ctx.fillStyle = "#FFFFFF";
-    // Reset the square value
-    square = c.width / 3;
+    length = c.width / 2;
 
-    // Retrieve and display the selected iteration count
     document.getElementById("iterationDisplay").innerHTML = iterationChoice;
     iterations = parseInt(iterationChoice);
-    // Start the recursion
-    iteration(square, square, 1);
+    iteration(length, length, 1);
 }
-*/
 
-// Begin the recursive function with the initial square
+
+// Begin the recursive function with the initial length
 iteration(length, length, 1);
