52444755582f46d768095fe93e4bf074b6b0c23b
Your name <lowejosh50@gmail.com>
2017-09-18 12:25:14 +1000

    refactored

--- workspace/js/sierpinski-carpet.js
+++ workspace/js/sierpinski-carpet.js
@@ -9,31 +9,21 @@ var square = c.width / 3;
 
 // Recursive function that calls upon itself until it reaches the set amount of iterations
 function iteration(x, y, iterationCount) {
-    // If the iteration count is yet to reach the set iterations
     if (iterationCount != iterations + 1) {
-
-        // Retrieve the square size and draw it
         square = getSquareSize(iterationCount);
         ctx.fillRect(x, y, square, square);
         
-        // For every sub square
         for (var i = 0; i < 8; i++) {
-            // If the current iteration isn't the last
             if (iterationCount + 1 != iterations + 1) {
-                // Retrieve the square size again for sanity
                 square = getSquareSize(iterationCount + 1);
-                // Return each x and y value for each square position
-                var rx = getParamX(i, square, x);
-                var ry = getParamY(i, square, y);
-                // Recursively call the function for the next iteration
-                iteration(rx, ry, iterationCount + 1);
+                iteration(getParamX(i, x), getParamY(i, y), iterationCount + 1);
             }
         } 
     }
 }
 
 // Returns the x value for proper square placement
-function getParamX(i, squareSize, x) {
+function getParamX(i, x) {
        if (i == 0 || i == 3 || i == 5) {
             return (x - (2 * square));
        } else if (i == 1 || i == 6) {
@@ -44,7 +34,7 @@ function getParamX(i, squareSize, x) {
 }
 
 // Returns the y value for proper square placement
-function getParamY(i, squareSize, y) {
+function getParamY(i, y) {
        if (i < 3) {
             return (y - (2 * square));
        } else if (i < 5) {
@@ -61,15 +51,10 @@ function getSquareSize(iterationCount) {
 
 // Redraws the sierpinski carpet with a new iteration setting
 function reDraw(iterationChoice) {
-    // Clear the screen
     ctx.clearRect(0, 0, c.width, c.height);
-    // Reset the square value
     square = c.width / 3;
-
-    // Retrieve and display the selected iteration count
     document.getElementById("iterationDisplay").innerHTML = iterationChoice;
     iterations = parseInt(iterationChoice);
-    // Start the recursion
     iteration(square, square, 1);
 }
 
