526e8b49670269ab8ca0ce1e0308a3cb3d3eb5d8
Your name <lowejosh50@gmail.com>
2017-09-21 09:21:48 +1000

    implemented the algorithm - no visualization

--- workspace/js/merge-sort-vis.js
+++ workspace/js/merge-sort-vis.js
@@ -3,29 +3,42 @@
 //var ctx = c.getContext("2d");
 //ctx.fillStyle = "#FFFFFF";
 
+// Recursively splits the array and merges them again after they've been ordered
 function mergeSort(array) {
-    // Initial Vars
-    var half = Math.ceil(array.length / 2); 
-    var leftSide = array.slice(0, half);
-    var rightSide = array.slice(half, array.length);
-    var i = 0;
-    var j = 0;
-    var results = [];
-    
-    while (i < leftSide.length || j < rightSide.length) {
-        if (i == leftSide.length) {
-            // j is the only index leftSide
-            results.push(rightSide[j]);
-            j++;
-        } else if (j == rightSide.length || leftSide[i] <= rightSide[j]) {
-            results.push(leftSide[i]);
-            i++;
-        } else {
-            results.push(rightSide[j]);
-            j++;
-        }
+    if (array.length == 1) {
+        return array;
     }
-    return results;
+
+    const half = Math.ceil(array.length / 2);
+    const leftSide = array.slice(0, half);
+    const rightSide = array.slice(half);
+    return sort(mergeSort(leftSide), mergeSort(rightSide));
 }
 
-console.log(mergeSort([1,3,4,3,7,9]));
+// Sorts the two sides into order
+function sort(leftSide, rightSide) {
+    let results = [];   // ordered result array
+    let i = 0;          // left side index
+    let ii = 0;         // right side index
+
+    // while the left index and right index have not cycled through
+    while (i < leftSide.length && ii < rightSide.length) {
+        // if the left number is smaller
+        if (leftSide[i] < rightSide[ii]) {
+            // push the left number to the result array
+            results.push(leftSide[i]);
+            // move onto the next left index
+            i++;
+        // if the right number is smaller
+        } else {
+            // push the right number to the result array
+            results.push(rightSide[ii]);
+            // move onto the next right number
+            ii++;
+        }
+    }
+
+    return results.concat(leftSide.slice(i)).concat(rightSide.slice(ii));
+}
+
+console.log(mergeSort([23,2,3,414,23,3,1,2,6,8,9,6,4,33]));
