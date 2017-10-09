21911aa41b83e9923be29193409c66ffd1a87631
Your name <lowejosh50@gmail.com>
2017-10-10 00:55:33 +1000

    started voroni diagrams - i feel like it will end up crashing in the end

---
+++ workspace/js/voroni-diagram.js
@@ -0,0 +1,37 @@
+// Canvas settings
+let c = document.getElementById("voroniDiagram"); 
+c.setAttribute('height', window.innerHeight - 64);
+c.setAttribute('width', window.innerWidth - 190); 
+let ctx = c.getContext("2d");
+ctx.fillStyle = "#FFFFFF";
+
+// Vars
+const NUM_OF_POINTS = 15;
+let pointArray = new Array(NUM_OF_POINTS);
+
+// Randomly generate co-ordinates and a color for every point
+for (let i = 0; i < NUM_OF_POINTS; i++) {
+    pointArray[i] = new Array(3);
+    pointArray[i][0] = Math.floor(Math.random() * c.width);
+    pointArray[i][1] = Math.floor(Math.random() * c.height);
+    pointArray[i][2] = randomColor();
+}
+
+// Brute force through every pixel and every point to find the closest (i know...)
+for (let i = 0; i < c.width; i++) {
+    for (let ii = 0; ii < c.height; ii++) {
+        // Initial value
+        let closestPoint = pointArray[0];
+        let closestDist = Math.sqrt((pointArray[0][0] - i) + (pointArray[0][1] - ii));
+        console.log(closestDist);
+        for (let iii = 0; iii < NUM_OF_POINTS; i++) {
+             
+        }    
+    }
+}
+
+// Random Colors
+function randomColor() {
+    return '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+}
+
