0cb150e29b14842e802787579cee6f8751c20ee4
Your name <lowejosh50@gmail.com>
2017-11-17 02:23:41 +1000

    touchups

--- workspace/js/pattern-1.js
+++ workspace/js/pattern-1.js
@@ -5,11 +5,10 @@ c.setAttribute('width', window.innerHeight - 64); // Needs to be square-shaped
 let ctx = c.getContext("2d");
 
 // Vars
-let inc = 0.01;
-let scale = 150;
 let center = c.width / 2;
 let fillInput = document.getElementById("chosenColor");
 let bgInput = document.getElementById("backgroundColor");
+let isColorRandom = false;
 fillInput.value = "#FCFCFC";
 bgInput.value = "#1B1B1B";
 
@@ -17,33 +16,27 @@ bgInput.value = "#1B1B1B";
 function drawRose() {
     // Clear the screen
     ctx.clearRect(0, 0, c.width, c.height);
-    let n = getN(); 
-    let d = getD();
-    updateSidebar(n, d);
+
+    // Retrieve parameters
+    let n = parseInt(document.getElementById("nChoice").value);
+    let d = parseInt(document.getElementById("dChoice").value);
+    let inc = parseFloat(document.getElementById("incChoice").value);
+    let scale = parseInt(document.getElementById("scaleChoice").value);
+    let size = parseInt(document.getElementById("thickChoice").value);
+    updateSidebar(n, d, inc, scale, size);
+
+    // Actually draw
     let k = n / d;
+    ctx.fillStyle = fillInput.value;
     for (let i = 0; i < Math.PI * d * 2; i+=inc) {
         let r = scale * Math.cos(k * i);
         let x = r * Math.cos(i);
         let y = r * Math.sin(i);
-        ctx.fillStyle = fillInput.value;
-        ctx.fillRect(center + x, center + y, 1, 1);
+        if (isColorRandom) {
+            ctx.fillStyle = randomColor();
+        }
+        ctx.fillRect(center + x, center + y, size, size);
     }
-}
-
-// Updates the sidebar values
-function updateSidebar(n, d) {
-    document.getElementById("nDisplay").innerHTML = n;
-    document.getElementById("dDisplay").innerHTML = d;
-}
-
-// Retrieve d value 
-function getD() {
-    return document.getElementById("dChoice").value;
-}
-
-// Retrieve n value 
-function getN() {
-    return document.getElementById("nChoice").value;
 }
 
 // Run the initial script
@@ -52,8 +45,18 @@ drawRose(5, 8, 150, 0.01, fillInput.value);
 
 // ---------- SETTING FUNCTIONS ----------
 
+// Updates the sidebar values
+function updateSidebar(n, d, inc, scale, size) {
+    document.getElementById("nDisplay").innerHTML = n;
+    document.getElementById("dDisplay").innerHTML = d;
+    document.getElementById("incDisplay").innerHTML = inc;
+    document.getElementById("scaleDisplay").innerHTML = scale;
+    document.getElementById("thickDisplay").innerHTML = size;
+}
+
 // Fill Color Picker
 fillInput.addEventListener("change", function() {
+    isColorRandom = false;
     drawRose();
 }, false);
 
@@ -64,7 +67,13 @@ bgInput.addEventListener("change", function() {
 }, false);
 
 // Random Colors
+function randomColors() {
+    isColorRandom = true;
+    drawRose();
+}
+
 function randomColor() {
     return '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
 }
+
 
