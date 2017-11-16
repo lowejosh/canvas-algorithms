b728d44277a83899148e5eb6c62f2768f3dbea64
Your name <lowejosh50@gmail.com>
2017-11-16 18:36:43 +1000

    rose working

--- workspace/js/pattern-1.js
+++ workspace/js/pattern-1.js
@@ -7,14 +7,20 @@ ctx.fillStyle = "#FCFCFC";
 
 // Vars
 let inc = 0.02;
-let n = 5;
 let d = 8;
+let n = 5;
+let scale = 100;
+let center = c.width / 2;
 
 // Main function
 function main() {
     let k = n / d;
-    for (let i < 0; i < Math.Pi * d; i+=inc) {
-    
+    for (let i = 0; i < Math.PI * d * 2; i+=inc) {
+        console.log('reached');
+        let r = scale * Math.cos(k * i);
+        let x = r * Math.cos(i)
+        let y = r * Math.sin(i)
+        ctx.fillRect(center + x, center + y, 1, 1);
     }
 
 }
